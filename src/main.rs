1a9c324c0a55d764acc0c0e7dfdf4486f7d33bfa
Shane Celis <shane.celis@gmail.com>
2024-12-05 06:13:09 -0500

    feature: It is feature complete.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@ use serde::{Deserialize, Serialize};
 use satoxid::{CadicalEncoder, constraints::{Or, ExactlyK, Not, And, If}, Encoder, Backend};
 use std::{
     cmp::Reverse,
-    fmt::Write,
+    fmt::{self, Debug, Write},
     fs::File,
     path::PathBuf,
     iter,
-    fmt::Debug,
     hash::Hash,
 };
 use clap::{Args, Parser, Subcommand, ValueEnum};
 use ron::ser::PrettyConfig;
+use cmd_lib::run_cmd;
 
 #[derive(Parser, Debug)]
 struct Cli {
     #[arg(long)]
     write_default: bool,
+    /// Execute a command: cat $body | $exec -s "$subject" "First <name@email.com>"
+    #[arg(long)]
+    exec: Option<String>,
+    /// Dry run, won't execute command, will echo it: cat $body; echo $exec -s "$subject" "First <name@email.com>"
+    #[arg(long, short = 'n', default_value_t = false)]
+    dry_run: bool,
     /// The path to read
     #[arg(required = true, value_name = "FILE", value_hint = clap::ValueHint::FilePath)]
     input: PathBuf,
@@ -24,7 +30,7 @@ struct Cli {
 #[derive(Debug, Clone, Deserialize, Serialize, Default)]
 struct Solution {
     year: u16,
-    enable: bool,
+    exclude_pairs: bool,
     pairs: Vec<Pair<String>>,
 }
 
@@ -129,7 +135,80 @@ fn exclude_sets<T: Debug + Eq + Hash + PartialEq + Clone>(people: &[T],
     exclude_pairs_symmetric(accum.into_iter(), encoder);
 }
 
-fn main() {
+#[derive(Debug)]
+struct Message {
+    subject: String,
+    body: String,
+    email: String,
+}
+
+/// Return the givers for this person.
+fn givers_for<'a>(receiver: &'a str, input: &'a Input) -> impl Iterator<Item = String> + use<'a> {
+    input.history.iter().flat_map(move |x| x.pairs.iter().filter(move |p| p.receiver == receiver).map(|p|
+                                                                                                      format!("{} ({})", p.giver, x.year)))
+}
+
+/// Return the receivers for this person.
+fn receivers_for<'a>(giver: &'a str, input: &'a Input) -> impl Iterator<Item = String> + use<'a> {
+    input.history.iter().flat_map(move |x| x.pairs.iter().filter(move |p| p.giver == giver).map(|p|
+                                                                                                format!("{} ({})", p.receiver, x.year)))
+}
+
+fn compose_message(pair: &Pair<String>, input: &Input) -> Result<Message, fmt::Error> {
+    let giver = &pair.giver;
+    let receiver = &pair.receiver;
+    let subject = format!("Secret Santa {giver}: Keep it secret! Keep it safe!");
+    let mut body = String::new();
+    writeln!(body, "{giver}, you are the Secret Santa for {receiver}.")?;
+
+    let mut receivers = receivers_for(giver, input).peekable();
+
+    if receivers.peek().is_some() {
+        writeln!(body, "")?;
+        write!(body, "You were Secret Santa for ")?;
+        write!(body, "{}", receivers.next().unwrap())?;
+        while let Some(receiver) = receivers.next() {
+            if receivers.peek().is_none() {
+                write!(body, ", and {}", receiver)?;
+            } else {
+                write!(body, ", {}", receiver)?;
+            }
+        }
+        writeln!(body, ".")?;
+    }
+
+    let mut givers = givers_for(giver, input).peekable();
+
+    if givers.peek().is_some() {
+        writeln!(body, "")?;
+        write!(body, "You had these Secret Santas in Christmases past: ")?;
+        write!(body, "{}", givers.next().unwrap())?;
+        // for giver in givers {
+        //     write!(body, ", {}", giver);
+        // }
+        while let Some(giver) = givers.next() {
+            if givers.peek().is_none() {
+                write!(body, ", and {}", giver)?;
+            } else {
+                write!(body, ", {}", giver)?;
+            }
+        }
+        writeln!(body, ".")?;
+    }
+
+    writeln!(body, r#"
+* * *
+Brought to you by secret-santa[1].
+
+[1]: https://github.com/shanecelis/secret-santa/blob/main/src/main.rs
+"#)?;
+
+    let email = input.people.iter().find(|p| p.name == pair.giver).expect("Failed to find email address").email.clone();
+    let name_and_email = format!("{} <{}>", pair.giver, email);
+    Ok(Message { subject, body, email: name_and_email })
+}
+
+fn main() -> std::io::Result<()>{
     let cli = Cli::parse();
 
     if cli.write_default {
@@ -143,13 +222,13 @@ fn main() {
         input.blacklist_sets.push(vec![a.name.clone(), b.name.clone()]);
         input.whitelist.push(Pair::new(b.name.clone(), c.name.clone()));
         input.blacklist.push(Pair::new(b.name.clone(), c.name.clone()));
-        input.history.push(Solution { year: 2024, enable: true,
+        input.history.push(Solution { year: 2024, exclude_pairs: true,
                                       pairs: vec![Pair::new(a.name.clone(), c.name.clone()),
                                                   Pair::new(b.name.clone(), a.name.clone()),
                                                   Pair::new(c.name.clone(), b.name.clone())]});
         // TODO: Should use a stream here.
         println!("{}", ron::ser::to_string_pretty(&input, PrettyConfig::default()).unwrap());
-        return;
+        return Ok(());
     }
 
     let f = File::open(cli.input).expect("Failed opening");
@@ -159,13 +238,7 @@ fn main() {
 
     let mut encoder = CadicalEncoder::new();
     input.history.sort_by_key(|sol| Reverse(sol.year));
-
-
     let names: Vec<String> = input.people.iter().map(|p| p.name.clone()).collect();
-    // let people: Vec<u8> = (0..4).collect();
-
-    // encoder.add_constraint(Or(iter::once(Pair { giver: 0u8, receiver: 1u8 })));
-
     encode_secret_santa_rules(&names, &mut encoder);
     for blacklist_set in &input.blacklist_sets {
         exclude_sets(blacklist_set, &mut encoder);
@@ -173,104 +246,50 @@ fn main() {
     exclude_pairs(input.blacklist.iter().cloned(), &mut encoder);
     include_pairs(input.whitelist.iter().cloned(), &mut encoder);
 
+    // Exclude historical pairs.
+    for solution in &input.history {
+        if ! solution.exclude_pairs {
+            continue;
+        }
+        exclude_pairs(solution.pairs.iter().cloned(), &mut encoder);
+    }
+
     if let Some(model) = encoder.solve() {
 
         let mut pairs: Vec<Pair<String>> = model.vars().filter_map(|v| v.is_pos().then(|| v.unwrap())).collect();
         pairs.sort_by_key(|p| p.giver.clone());
 
-
+        let mut msgs = vec![];
+        /// Generate all the messages first to confirm there aren't any errors.
         for pair in &pairs {
             println!("{:?}", pair);
-            println!("{}", compose_message(pair, &input).body);
+            let msg = compose_message(pair, &input).expect("Failed to compose message");
+            msgs.push(msg);
         }
-    }
-}
 
-#[derive(Debug)]
-struct Message {
-    subject: String,
-    body: String,
-}
+        for msg in msgs {
+            let subject = msg.subject;
+            let body = msg.body;
+            let email = msg.email;
+            let arg = r"1 2 3";
 
-/// Return the givers for this person.
-fn givers_for<'a>(receiver: &'a str, input: &'a Input) -> impl Iterator<Item = String> + use<'a> {
-    input.history.iter().flat_map(move |x| x.pairs.iter().filter(move |p| p.receiver == receiver).map(|p|
-                                                                                                      format!("{} ({})", p.giver, x.year)))
-}
+            if let Some(ref exec) = cli.exec {
+                let exec_args: Vec<&str> = exec.split_whitespace().collect();
 
-/// Return the receivers for this person.
-fn receivers_for<'a>(giver: &'a str, input: &'a Input) -> impl Iterator<Item = String> + use<'a> {
-    input.history.iter().flat_map(move |x| x.pairs.iter().filter(move |p| p.giver == giver).map(|p|
-                                                                                                      format!("{} ({})", p.receiver, x.year)))
-}
-
-fn compose_message(pair: &Pair<String>, input: &Input) -> Message {
-    let giver = &pair.giver;
-    let receiver = &pair.receiver;
-    let subject = format!("Secret Santa {giver}: Keep it secret! Keep it safe!");
-    let mut body = String::new();
-    writeln!(body, "{giver}, you are the Secret Santa for {receiver}.");
-
-    let mut receivers = receivers_for(giver, input).peekable();
-
-    if receivers.peek().is_some() {
-        writeln!(body, "");
-        write!(body, "You were Secret Santa for ");
-        write!(body, "{}", receivers.next().unwrap());
-        while let Some(receiver) = receivers.next() {
-            if receivers.peek().is_none() {
-                write!(body, ", and {}", receiver);
-            } else {
-                write!(body, ", {}", receiver);
+                if cli.dry_run {
+                    run_cmd!(echo $body | cat; echo $[exec_args] -s $subject $email)?;
+                } else {
+                    run_cmd!(echo $body | $[exec_args] -s $subject $email)?;
+                }
             }
         }
-        writeln!(body, ".");
+    } else {
+        eprintln!("No secret santa solutions found!");
+        std::process::exit(1);
     }
-
-    let mut givers = givers_for(giver, input).peekable();
-
-    if givers.peek().is_some() {
-        writeln!(body, "");
-        write!(body, "You had these Secret Santas in Christmases past: ");
-        write!(body, "{}", givers.next().unwrap());
-        // for giver in givers {
-        //     write!(body, ", {}", giver);
-        // }
-        while let Some(giver) = givers.next() {
-            if givers.peek().is_none() {
-                write!(body, ", and {}", giver);
-            } else {
-                write!(body, ", {}", giver);
-            }
-        }
-        writeln!(body, ".");
-    }
-
-
-          // mailings.Add(() => Mail(email[x], $"Secret Santa {name}: Keep it secret! Keep it safe!",
-          //                         $"{x}, you are the Secret Santa for {y}.\n"
-          //                         + (LastSantaees(x, history).Any() ?
-          //                          $"\nYou were Secret Santa for " +
-          //                          string.Join(", ", LastSantaees(x, history)
-          //                                      .OrderByDescending(t => int.Parse(t.Item2))
-          //                                      .Select(t => $"{t.Item1} ({t.Item2})"))
-          //                          + ".\n"
-          //                          : "")
-          //                         +
-          //                         (LastSecretSantas(x, history).Any()
-          //                          ? $"\nYou had these Secret Santas in Christmases past: " +
-          //                          string.Join(", ", LastSecretSantas(x, history)
-          //                                      .OrderByDescending(t => int.Parse(t.Item2))
-          //                                      .Select(t => $"{t.Item1} ({t.Item2})")) + ".\n"
-          //                          : "")
-          //                         + "\n"
-          //                         + "* * *\n"
-          //                         + "\n"
-          //                         + "Brought to you by SecretSanta.cs[1].\n"
-          //                         + "\n"
-          //                         + "[1]: https://gist.github.com/shanecelis/b88808f5198832dd5f3dd2015017f0ec\n"));
-    Message { subject, body }
+    Ok(())
 }
+
 
 #[cfg(test)]
 mod test {
@@ -289,3 +308,31 @@ mod test {
         assert_eq!(ron::ser::to_string(&p).unwrap(), "(name:\"First Last\",email:\"name@email.com\")");
     }
 }
+
+// TODO: Add these subcommands next year.
+//
+// #[derive(Debug, Subcommand)]
+// enum Subcommands {
+//     /// Write creature to file path
+//     #[command(arg_required_else_help = true)]
+//     Generate {
+//         #[arg(required = true, value_name = "FILE", value_hint = clap::ValueHint::FilePath)]
+//         input: PathBuf,
+//         /// The path to write
+//         #[arg(required = true, value_name = "FILE", value_hint = clap::ValueHint::FilePath)]
+//         output: PathBuf,
+//     },
+//     Execute {
+//         #[arg(required = true, value_name = "FILE", value_hint = clap::ValueHint::FilePath)]
+//         input: PathBuf,
+//         /// The path to write
+//         #[arg(required = true, value_name = "FILE", value_hint = clap::ValueHint::FilePath)]
+//         output: PathBuf,
+//     },
+//     #[command(arg_required_else_help = true)]
+//     Read {
+//         /// The path to read
+//         #[arg(required = true, value_name = "FILE", value_hint = clap::ValueHint::FilePath)]
+//         path: PathBuf,
+//     },
+// }
