d6f4a262d42363d782b8c5877af049265190eb7f
Shane Celis <shane.celis@gmail.com>
2024-12-05 04:54:51 -0500

    feature: Compose message.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use serde::{Deserialize, Serialize};
 use satoxid::{CadicalEncoder, constraints::{Or, ExactlyK, Not, And, If}, Encoder, Backend};
 use std::{
+    cmp::Reverse,
+    fmt::Write,
+    fs::File,
     path::PathBuf,
     iter,
     fmt::Debug,
@@ -20,7 +23,8 @@ struct Cli {
 
 #[derive(Debug, Clone, Deserialize, Serialize, Default)]
 struct Solution {
-    note: String,
+    year: u16,
+    enable: bool,
     pairs: Vec<Pair<String>>,
 }
 
@@ -30,8 +34,24 @@ struct Input {
     whitelist: Vec<Pair<String>>,
     blacklist: Vec<Pair<String>>,
     blacklist_sets: Vec<Vec<String>>,
-    blacklist_symmetric: Vec<Pair<String>>,
     history: Vec<Solution>,
+}
+
+impl Input {
+    /// Confirm all names present are in the people list.
+    fn check_history(&self) {
+        for solution in &self.history {
+            for pair in &solution.pairs {
+                if self.people.iter().find(|p| p.name == pair.giver).is_none() {
+                    panic!("Giver named '{}' present in history but not found in people set.", &pair.giver);
+                }
+
+                if self.people.iter().find(|p| p.name == pair.receiver).is_none() {
+                    panic!("Receiver named '{}' present in history but not found in people set.", &pair.receiver);
+                }
+            }
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize, Serialize)]
@@ -81,6 +101,11 @@ fn encode_secret_santa_rules<T: Debug + Eq + Hash + PartialEq + Clone>(universe:
     }
 }
 
+fn include_pairs<T: Debug + Eq + Hash + PartialEq + Clone>(lits: impl Iterator<Item = Pair<T>> + Clone,
+                                                           encoder: &mut Encoder<Pair<T>, impl Backend>) {
+    encoder.add_constraint(And(lits));
+}
+
 fn exclude_pairs<T: Debug + Eq + Hash + PartialEq + Clone>(lits: impl Iterator<Item = Pair<T>> + Clone,
                                                            encoder: &mut Encoder<Pair<T>, impl Backend>) {
     encoder.add_constraint(Not(Or(lits)));
@@ -92,7 +117,7 @@ fn exclude_pairs_symmetric<T: Debug + Eq + Hash + PartialEq + Clone>(lits: impl 
     exclude_pairs(lits.map(|Pair { giver: a, receiver: b }| Pair { giver: b, receiver: a }), encoder);
 }
 
-fn exclude_sets<T: Debug + Eq + Hash + PartialEq + Clone>(people: Vec<T>,
+fn exclude_sets<T: Debug + Eq + Hash + PartialEq + Clone>(people: &[T],
                                                           encoder: &mut Encoder<Pair<T>, impl Backend>) {
     let len = people.len();
     let mut accum = vec![];
@@ -118,38 +143,139 @@ fn main() {
         input.blacklist_sets.push(vec![a.name.clone(), b.name.clone()]);
         input.whitelist.push(Pair::new(b.name.clone(), c.name.clone()));
         input.blacklist.push(Pair::new(b.name.clone(), c.name.clone()));
-        input.blacklist_symmetric.push(Pair::new(b.name.clone(), c.name.clone()));
-        input.history.push(Solution { note: "Year 2024".into(), pairs: vec![Pair::new(a.name.clone(), c.name.clone()),
-                                                                            Pair::new(b.name.clone(), a.name.clone()),
-                                                                            Pair::new(c.name.clone(), b.name.clone())]});
+        input.history.push(Solution { year: 2024, enable: true,
+                                      pairs: vec![Pair::new(a.name.clone(), c.name.clone()),
+                                                  Pair::new(b.name.clone(), a.name.clone()),
+                                                  Pair::new(c.name.clone(), b.name.clone())]});
         // TODO: Should use a stream here.
         println!("{}", ron::ser::to_string_pretty(&input, PrettyConfig::default()).unwrap());
         return;
     }
 
+    let f = File::open(cli.input).expect("Failed opening");
+
+    let mut input: Input = ron::de::from_reader(f).expect("Failed parsing");
+    input.check_history();
+
     let mut encoder = CadicalEncoder::new();
+    input.history.sort_by_key(|sol| Reverse(sol.year));
 
-    let people: Vec<u8> = (0..4).collect();
 
-    encoder.add_constraint(Or(iter::once(Pair { giver: 0u8, receiver: 1u8 })));
+    let names: Vec<String> = input.people.iter().map(|p| p.name.clone()).collect();
+    // let people: Vec<u8> = (0..4).collect();
 
-    encode_secret_santa_rules(&people, &mut encoder);
+    // encoder.add_constraint(Or(iter::once(Pair { giver: 0u8, receiver: 1u8 })));
+
+    encode_secret_santa_rules(&names, &mut encoder);
+    for blacklist_set in &input.blacklist_sets {
+        exclude_sets(blacklist_set, &mut encoder);
+    }
+    exclude_pairs(input.blacklist.iter().cloned(), &mut encoder);
+    include_pairs(input.whitelist.iter().cloned(), &mut encoder);
 
     if let Some(model) = encoder.solve() {
 
-        for var in model.vars() {
-            // println!("{:?} {}", var, var.is_pos());
-            if var.is_pos() {
-                println!("{:?}", var);
-            }
+        let mut pairs: Vec<Pair<String>> = model.vars().filter_map(|v| v.is_pos().then(|| v.unwrap())).collect();
+        pairs.sort_by_key(|p| p.giver.clone());
+
+
+        for pair in &pairs {
+            println!("{:?}", pair);
+            println!("{}", compose_message(pair, &input).body);
         }
     }
+}
+
+#[derive(Debug)]
+struct Message {
+    subject: String,
+    body: String,
+}
+
+/// Return the givers for this person.
+fn givers_for<'a>(receiver: &'a str, input: &'a Input) -> impl Iterator<Item = String> + use<'a> {
+    input.history.iter().flat_map(move |x| x.pairs.iter().filter(move |p| p.receiver == receiver).map(|p|
+                                                                                                      format!("{} ({})", p.giver, x.year)))
+}
+
+/// Return the receivers for this person.
+fn receivers_for<'a>(giver: &'a str, input: &'a Input) -> impl Iterator<Item = String> + use<'a> {
+    input.history.iter().flat_map(move |x| x.pairs.iter().filter(move |p| p.giver == giver).map(|p|
+                                                                                                      format!("{} ({})", p.receiver, x.year)))
+}
+
+fn compose_message(pair: &Pair<String>, input: &Input) -> Message {
+    let giver = &pair.giver;
+    let receiver = &pair.receiver;
+    let subject = format!("Secret Santa {giver}: Keep it secret! Keep it safe!");
+    let mut body = String::new();
+    writeln!(body, "{giver}, you are the Secret Santa for {receiver}.");
+
+    let mut receivers = receivers_for(giver, input).peekable();
+
+    if receivers.peek().is_some() {
+        writeln!(body, "");
+        write!(body, "You were Secret Santa for ");
+        write!(body, "{}", receivers.next().unwrap());
+        while let Some(receiver) = receivers.next() {
+            if receivers.peek().is_none() {
+                write!(body, ", and {}", receiver);
+            } else {
+                write!(body, ", {}", receiver);
+            }
+        }
+        writeln!(body, ".");
+    }
+
+    let mut givers = givers_for(giver, input).peekable();
+
+    if givers.peek().is_some() {
+        writeln!(body, "");
+        write!(body, "You had these Secret Santas in Christmases past: ");
+        write!(body, "{}", givers.next().unwrap());
+        // for giver in givers {
+        //     write!(body, ", {}", giver);
+        // }
+        while let Some(giver) = givers.next() {
+            if givers.peek().is_none() {
+                write!(body, ", and {}", giver);
+            } else {
+                write!(body, ", {}", giver);
+            }
+        }
+        writeln!(body, ".");
+    }
+
+
+          // mailings.Add(() => Mail(email[x], $"Secret Santa {name}: Keep it secret! Keep it safe!",
+          //                         $"{x}, you are the Secret Santa for {y}.\n"
+          //                         + (LastSantaees(x, history).Any() ?
+          //                          $"\nYou were Secret Santa for " +
+          //                          string.Join(", ", LastSantaees(x, history)
+          //                                      .OrderByDescending(t => int.Parse(t.Item2))
+          //                                      .Select(t => $"{t.Item1} ({t.Item2})"))
+          //                          + ".\n"
+          //                          : "")
+          //                         +
+          //                         (LastSecretSantas(x, history).Any()
+          //                          ? $"\nYou had these Secret Santas in Christmases past: " +
+          //                          string.Join(", ", LastSecretSantas(x, history)
+          //                                      .OrderByDescending(t => int.Parse(t.Item2))
+          //                                      .Select(t => $"{t.Item1} ({t.Item2})")) + ".\n"
+          //                          : "")
+          //                         + "\n"
+          //                         + "* * *\n"
+          //                         + "\n"
+          //                         + "Brought to you by SecretSanta.cs[1].\n"
+          //                         + "\n"
+          //                         + "[1]: https://gist.github.com/shanecelis/b88808f5198832dd5f3dd2015017f0ec\n"));
+    Message { subject, body }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use ron::ser::PrettyConfig;
+
 
     fn p0() -> Person {
         Person { name: String::from("First Last"), email: String::from("name@email.com") }
